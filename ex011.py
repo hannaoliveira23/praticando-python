19622900a9b10a0b2655443ebf500365dd1088d1
Hanna Oliveira <hannaos2007@gmail.com>
2026-05-17 17:51:23 -0300

    Delete ex011.py

--- ex011.py
+++
@@ -1,8 +0,0 @@
-b = float(input('Digite a largura da parede: '))
-h = float(input('Digite a altura da parede: '))
-
-area = b*h
-# 1l de tinta = 2m^2
-tinta = area/2
-
-print('Para pintar a parede inteira, você precisa de {} litro(s) de tinta.'.format(tinta))
