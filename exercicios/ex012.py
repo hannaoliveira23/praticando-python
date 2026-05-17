804fe78a1c8a28be4f375f43437e38ac9dbb28ab
Hanna Oliveira <hannaos2007@gmail.com>
2026-05-17 18:31:49 -0300

    Formatação da quantidade de casas decimais da saída

--- exercicios/ex012.py
+++ exercicios/ex012.py
@@ -1,2 +1,2 @@
 preco = float(input('Digite o preço do produto: R$'))
-print('O preço do produto após desconto de 5% é R${}.'.format(preco-((preco*5)/100)))
+print('O preço do produto após desconto de 5% é R${:.2f}.'.format(preco-((preco*5)/100)))
