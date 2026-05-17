6b3bcc10f0f34e2aa672c629cfa8e1196a852fdd
Hanna Oliveira <hannaos2007@gmail.com>
2026-05-17 18:00:16 -0300

    Update ex006.py

--- exercicios/ex006.py
+++ exercicios/ex006.py
@@ -1,2 +1,2 @@
 num = int(input('Digite um número: '))
-print('Dobro: {}, triplo: {}, raiz quadrada: {}.'.format(num*2, num*3, num**(1/2)))
+print('Dobro: {}, triplo: {}, raiz quadrada: {:.2f}.'.format(num*2, num*3, num**(1/2)))
