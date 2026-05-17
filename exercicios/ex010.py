474901b0b23c8f2da40e08f70ecd63035b2de62b
Hanna Oliveira <hannaos2007@gmail.com>
2026-05-17 18:21:44 -0300

    Correção do código e atualização da cotação do dólar

--- exercicios/ex010.py
+++ exercicios/ex010.py
@@ -1,3 +1,3 @@
-# Considerando US$1.00 = R$3.27
-dinheiro = float(input('Quanto dinheiro você tem na carteira? (em reais) '))
-print('Isso equivale a {} dólares.'.format(dinheiro*3.27))
+# Considerando US$1.00 = R$5.06
+dinheiro = float(input('Quanto dinheiro você tem na carteira? R$'))
+print('Isso equivale a US${:.2f}.'.format(dinheiro/5.06))
