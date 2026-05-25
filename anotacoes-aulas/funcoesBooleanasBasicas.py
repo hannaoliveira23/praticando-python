e3e88e5784f9675b2c2be6559f3241020ffea45e
Hanna Oliveira <hannaos2007@gmail.com>
2026-05-24 21:57:08 -0300

    Create funcoesBooleanasBasicas.py

---
+++ anotacoes-aulas/funcoesBooleanasBasicas.py
@@ -0,0 +1,5 @@
+n = input('Digite algo: ')
+print(n.isnumeric()) # se pode ser convertido a número
+print(n.isalpha()) # se é alfabético
+print(n.isalnum()) # se é alfanumérico (letra(s) e/ou número(s))
+print(n.isupper()) # se a palavra inteira está em caps lock
