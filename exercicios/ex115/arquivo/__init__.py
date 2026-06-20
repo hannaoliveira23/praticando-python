e2c5ab2a9e05d2c41e7489307183b30e733aeade
Hanna Oliveira <hannaos2007@gmail.com>
2026-06-20 12:22:55 -0300

    Add file handling functions in __init__.py

---
+++ exercicios/ex115/arquivo/__init__.py
@@ -0,0 +1,53 @@
+from lib.interface import cabecalho
+
+
+def arquivoExiste(nome):
+    try:
+        a = open(nome, 'rt')
+        a.close()
+    except FileNotFoundError:
+        return False
+    else:
+        return True
+
+
+def criarArquivo(nome):
+    try:
+        a = open(nome, 'wt+')
+        a.close()
+    except:
+        print('\033[31mHouve um ERRO na criação do arquivo!\033[m')
+    else:
+        print(f'Arquivo {nome} criado com sucesso!')
+
+
+def lerArquivo(nome):
+    try:
+        a = open(nome, 'rt')
+    except:
+        print('\033[31mErro ao ler o arquivo!\033[m')
+    else:
+        cabecalho('PESSOAS CADASTRADAS')
+        for linha in a:
+            dado = linha.split(';') # dado[0] = nome e dado[1] = idade
+            dado[1] = dado[1].replace('\n', '')
+            print(f'{dado[0]:<26}{dado[1]:>3} anos')
+
+    finally:
+        a.close()
+
+
+def cadastrar(arq, nome='desconhecido', idade=0):
+    try:
+        a = open(arq, 'at')
+    except:
+        print('\033[31mHouve um ERRO na abertura do arquivo!\033[m')
+    else:
+        try:
+            a.write(f'{nome};{idade}\n')
+        except:
+            print('\033[31mHouve um ERRO na hora de escrever os dados!\033[m')
+        else:
+            print(f'Novo registro de {nome} adicionado.')
+    finally:
+        a.close()
